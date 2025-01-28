d44947134ba3c032c2b3206863d2b8ebb7506738
rufaida01 <rufaidamohammed01@gmail.com>
2025-01-28 03:07:58 +0200

    Add files via upload
    Python script to generate YouTube channel names

---
+++ program.py
@@ -0,0 +1,4 @@
+print("Welcome to the youtube channel name generator")
+name= input("What is your name? \n")
+topic= input("What is your channel about? \n")
+print(f"You could name your channel: {topic} with {name}.")
